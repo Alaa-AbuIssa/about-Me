4125fa9fdbf869bd55089f517a4291dceb655e70
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-03-29 15:09:57 +0300

    lab2 finished

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# about-Me
+## Lab- 2
+
+## Guessing Game 
+
+## Author: ALaa Abu Issa 
+
+
+
+
+### Comments:
+
+**How did this go, overall?**
+*Ummmm.. It's funnily enough*
+
+
+**What observations or questions do you have about what you’ve learned so far?**
+*every things under control until this moments alhamdolla*
+
+
+**How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?**
+*It took just an hour but to be honest I expected it to take much more than that*
---
+++ js/app.js
@@ -0,0 +1,72 @@
+'use strict';
+ 
+    alert ("Hello there!");
+   let userName =prompt ("Please Enter Your Name");
+   alert ("Dear M.r/Miss "+userName + "\n"+" Hope this messege finds you well ");
+  
+  
+  
+   let myTall=prompt("am 178 cm ? "+"\n"+"What do you think?");
+        // myTall=myTall.toLowerCase();
+
+
+    if (myTall==='yes'||myTall==='y'){
+        alert("amaizing!! go on to the next Q");
+        // console.log("amaizing!! go on to the next Q");
+    }
+    else{
+        alert("ops! keep on never give up");
+        // console.log("ops! keep on never give up");
+    }
+
+
+    let myFavoriteFood=prompt("It's maglobeh"+"\n"+"What do you think?");
+        myFavoriteFood=myFavoriteFood.toLowerCase();
+
+    if(myFavoriteFood==="yes"||myFavoriteFood==="y"){
+        alert("amaizing!! go on to the next Q");
+        // console.log("amaizing!! go on to the next Q");
+    }
+    else{
+        alert("ops! keep on never give up");
+        // console.log("ops! keep on never give up");
+    }
+
+    
+   let BestActor=prompt("jason statham "+"\n"+"What do you think?");
+   BestActor=BestActor.toLowerCase();
+
+   if(BestActor==="yes"||BestActor==="y")
+   {
+    alert("amaizing!! go on to the next Q");
+    // console.log("amaizing!! go on to the next Q");
+   }else{
+    alert("ops! keep on never give up");
+        // console.log("ops! keep on never give up");
+   }
+
+
+
+   let myBestCity=prompt("Roma "+"\n"+"What do you think?");
+   myBestCity=myBestCity.toLowerCase();
+   
+   if(myBestCity==="yes"||myBestCity==="y"){
+    alert("amaizing!! go on to the next Q");
+    // console.log("amaizing!! go on to the next Q");
+   }else{
+    alert("ops! keep on never give up");
+        // console.log("ops! keep on never give up");
+
+   }
+
+   let BestMajor=prompt("cyber security "+"\n"+"What do you think?");
+   BestMajor=BestMajor.toLowerCase();
+   if(BestMajor==="yes"||BestMajor==="y"){
+    alert("amaizing!! go on to the next Q");
+    // console.log("amaizing!! go on to the next Q");
+   }else{
+    alert("ops! keep on never give up");
+        // console.log("ops! keep on never give up");
+
+   }
+   alert("Thanks For Your Time M.r/Miss:  "+userName);
